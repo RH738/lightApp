fa6bf1c761d3462e68100023ac24eade81e98643
Rob738 <robertschvz@googlemail.com>
2016-07-04 19:52:06 +0200

    debugging

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  console.log("Someone is going to the website")
 });
-
+console.log("Hey Im running but stuff doesnt work for some reason")
 app.listen(3000 || process.env.port, function () {
   console.log('Example app listening on port 3000!');
 });
